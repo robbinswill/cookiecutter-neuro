08bcd564fc56d5ae67e5316c5874833951d3a292
wrobbins <will.robbins@dal.ca>
2021-08-14 19:25:28 -0300

    Started separate class to hold preprocessing logic.

--- {{ cookiecutter.project_slug }}/main.py
+++ {{ cookiecutter.project_slug }}/main.py
@@ -3,12 +3,12 @@ Driver script for cookiecutter-neuro. Provides a CLI for users.
 """
 
 from src.data import generate_bids
-from src.processing import preprocessing
+from src.processing import processing_interface
 
 import typer
 app = typer.Typer(help='{{ cookiecutter.project_name }} main interface')
 app.add_typer(generate_bids.app, name='bids')
-app.add_typer(preprocessing.app, name='processing')
+app.add_typer(processing_interface.app, name='processing')
 
 
 if __name__ == '__main__':
--- {{ cookiecutter.project_slug }}/src/processing/preprocessing.py
+++ {{ cookiecutter.project_slug }}/src/processing/preprocessing.py
@@ -1,16 +1,11 @@
 """
-This script contains the preprocessing logic
+Contains all of the preprocessing logic for the {{ cookiecutter.project_name }} project
 """
-
-import typer
-app = typer.Typer(help='Data preprocessing interface')
 
 import os
 from dotenv import find_dotenv, load_dotenv
 from pathlib import Path
-from nipype import Function, Node
 
-#todo could probably move these into config:
 
 # find .env automagically by walking up directories until it's found
 dotenv_path = find_dotenv()
@@ -22,40 +17,28 @@ DATASET_ROOT = Path(os.getenv("DATASET"))
 PROJECT_ROOT = Path(os.getenv("PROJECT"))
 
 
-def _get_raw(project_root: str, dataset_root: str, sub: str):
+class Preprocessing:
     """
-    Returns the MNE Raw object based on the given subject ID
+    To be used by the processing interface for implementing preprocessing steps
     """
-
-    from bids import BIDSLayout
-    from omegaconf import DictConfig, OmegaConf
-    from hydra import compose, initialize_config_dir
-    from hydra.utils import call
-
-    initialize_config_dir(config_dir=project_root)
-    cfg = compose("config.yaml")
-
-    layout = BIDSLayout(root=dataset_root)
-    raw_file = layout.get(subject=sub, extension=cfg['raw_extension'], suffix=cfg['data_type'],
-                          return_type='filename')[0]
-    return call(cfg.raw, vhdr_fname=raw_file)
+    def __init__(self):
+        self.project_root = PROJECT_ROOT.joinpath('src', 'conf').__str__()
+        self.dataset_root = DATASET_ROOT.joinpath('raw_data').__str__()
 
 
-@app.command()
-def preprocess_subject(sub: str):
-    """
-    Launch preprocessing step for the given subject
-    """
+    def _get_raw(self, sub: str):
+        """
+        Returns the MNE Raw object based on the given subject ID
+        """
+        from bids import BIDSLayout
+        from omegaconf import DictConfig, OmegaConf
+        from hydra import compose, initialize_config_dir
+        from hydra.utils import call
 
-    getraw = Node(Function(input_names=['project_root', 'dataset_root', 'sub'],
-                           output_names=['raw'],
-                           function=_get_raw),
-                  name='raw_node')
-    getraw.inputs.project_root = PROJECT_ROOT.joinpath('src', 'conf').__str__()
-    getraw.inputs.dataset_root = DATASET_ROOT.joinpath('raw_data').__str__()
-    getraw.inputs.sub = sub
-    getraw.run()
-    raw = getraw.result.outputs.raw
+        initialize_config_dir(config_dir=self.project_root)
+        cfg = compose("config.yaml")
 
-    # Later try reading in and filtering a raw object
-    print('done')
+        layout = BIDSLayout(root=self.dataset_root)
+        raw_file = layout.get(subject=sub, extension=cfg['raw_extension'], suffix=cfg['data_type'],
+                              return_type='filename')[0]
+        self.raw = call(cfg.raw, vhdr_fname=raw_file)
